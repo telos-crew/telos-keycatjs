b0cba1202792450f86e5177ff1ff3851635d68e2
kylanhurt <kylan.hurt@gmail.com>
2020-11-30 18:32:26 -0700

    Fix binary function exports

--- src/utils/window.ts
+++ src/utils/window.ts
@@ -36,7 +36,7 @@ export function makeWindowUrl(origin, path, data: IUrlData) {
   return url.href
 }
 
-function fromBinary(binary) {
+export const fromBinary = binary => {
   const bytes = new Uint8Array(binary.length)
   for (let i = 0; i < bytes.length; i++) {
     bytes[i] = binary.charCodeAt(i)
@@ -44,10 +44,10 @@ function fromBinary(binary) {
   return String.fromCharCode(...new Uint16Array(bytes.buffer))
 }
 
-function toBinary(string) {
-  const codeUnits = new Uint16Array(string.length)
+export const toBinary = str => {
+  const codeUnits = new Uint16Array(str.length)
   for (let i = 0; i < codeUnits.length; i++) {
-    codeUnits[i] = string.charCodeAt(i)
+    codeUnits[i] = str.charCodeAt(i)
   }
   return String.fromCharCode(...new Uint8Array(codeUnits.buffer))
 }
